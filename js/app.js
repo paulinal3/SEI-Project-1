69d61bae3a6831e9af7827c610dc36bc25e357da
paulinal3 <paulinal3@outlook.com>
2021-10-25 10:50:22 -0400

    manatee moving

--- js/app.js
+++ js/app.js
@@ -11,20 +11,15 @@
 // use Math.random(Math.floor) to randomly spawn food and trash
     // then use an if statement to block spawning of trash in the 1/3 of the x-axis and allow it on the other 2/3    
 
-const game = document.getElementById('gameCanvas')
-const ctx = gameCanvas.getContext('2d')
+const game = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
     
-// create manatee and add to canvas
-// manatee.onload = function () {
-    //     ctx.drawImage(manatee, 5, 15)
-    // }
-    
-const player = {
+let player = {
     x: 10,
     y: 25,
     width: 49,
     height: 23,
-    speed: 5,
+    speed: 10,
     alive: true
 }
     
@@ -36,59 +31,34 @@ function drawGameElem(img, dX, dY, dW, dH) {
 }
 
 function animate() {
-    // ctx. clearRect(0, 0, canvas.width, canvas.height)
+    ctx. clearRect(0, 0, canvas.width, canvas.height)
     drawGameElem(manatee, player.x, player.y, player.width, player.height)
-    // movementHandler()
     requestAnimationFrame(animate)
 }
 
-const keys = []
-
-// const movementHandler = (e) => {
-// switch (e.key) {
-//     // left arrow
-//     case ('ArrowLeft'):
-//         player.x -= player.speed
-//         break
-//     // up arrow
-//     case ('ArrowUp'):
-//         player.y -= player.speed
-//         break
-//     // right arrow
-//     case ('ArrowRight'):
-//         player.x += player.speed
-//         break
-//     // down arrow
-//     case ('ArrowDown'):
-//         player.y += player.speed
-//         break
-//     }
-// }
+let movePlayer = (e) => {
+switch (e.key) {
+    // left arrow
+    case ('ArrowLeft'):
+        player.x -= player.speed
+        break
+    // up arrow
+    case ('ArrowUp'):
+        player.y -= player.speed
+        break
+    // right arrow
+    case ('ArrowRight'):
+        player.x += player.speed
+        break
+    // down arrow
+    case ('ArrowDown'):
+        player.y += player.speed
+        break
+    }
+}
 
 animate()
-    
-    // function drawImage(src, x, y) {
-    //         this.src = src
-    //         this.x = x
-    //         this.y = y
-    //         this.alive = true
-    //     }
-    //     let player = drawImage(manatee, 5, 15)
-
-// function gameElem(x, y, color, width, height) {
-//     this.x = x
-//     this.y = y
-//     this.color = color
-//     this.width = width
-//     this.height = height
-//     this.alive = true
-
-//     this.render = function () {
-//         ctx.fillStyle = this.color
-//         ctx.fillRect(this.x, this.y, this.width, this.height)
-//     }
-// }
-        
+         
 // let player = new gameElem(10, 25, 'gray', 10, 10)
 // let food = new gameElem(200, 115, 'lightgreen', 10,35)
 // let trash = new gameElem(150, 35, 'red', 10, 20)
@@ -118,15 +88,6 @@ animate()
 //     player.render()
 // }
 
-window.addEventListener('keydown', function (e) {
-    keys[e.key] = true
-    console.log(keys)
-})
-
-window.addEventListener('keyup', function(e) {
-    delete keys[e.key]
-})
-
-// document.addEventListener('keydown', movementHandler)
+document.addEventListener('keydown', movePlayer)
 
 // let gameInterval = setInterval(gameLoop, 70)
