68e6470f97e0fa57f832224b8c39b078e4997b79
paulinal3 <paulinal3@outlook.com>
2021-10-26 16:08:37 -0400

    rendering infinite food

--- js/app.js
+++ js/app.js
@@ -1,16 +1,178 @@
-// elements on the game
-    // manatee
-    // food populating across
-    // trash falling down
-    // hunger bar filling up
-    // when manatee collides with food, the hunger bar is increased
-    // when hunger bar reaches full, game is won
-    // if manatee and trash collide, manatee receives one scar
-    // if manatee receives 3 scars, the game is over
+// const game = document.getElementById('canvas')
+// const ctx = canvas.getContext('2d')
+// const foodEaten = document.getElementById('btmRight')
+     
+// const manateeImg = new Image()
+// manateeImg.src = ('../js/images/manatee.png')
 
-// use Math.random(Math.floor) to randomly spawn food and trash
-    // then use an if statement to block spawning of trash in the 1/3 of the x-axis and allow it on the other 2/3    
+// function drawPlayer(img, dX, dY, dW, dH) {
+//     ctx.drawImage(img, dX, dY, dW, dH)
+//     this.alive = true
+// }
 
+// let player = {
+//     x: 25,
+//     y: 25,
+//     width: 25,
+//     height: 12,
+//     speed: 10,
+//     alive: true
+// }
+
+// const foodImg = new Image()
+// foodImg.src = ('../js/images/foodPixil.png')
+
+// function foodElem(url, x, y) {
+//     this.url = url
+//     this.x = x
+//     this.y = y
+//     this.alive = true
+
+//     this.render = function () {
+//         ctx.drawImage(this.url, this.x, this.y,)
+//     }
+// }
+
+// const foodNum = 5
+// let foodArr = []
+
+// // loop through empty food arr
+// // for (let i = 0; i < foodNum; i++) {
+// //     foodArr.push({
+// //         x: Math.random()*300
+// //         y: Math.random()*150
+// //     })
+// // }
+
+// const appleImg = new Image()
+// appleImg.src = ('../js/images/applePixil.png')
+
+// const sodaImg = new Image()
+// sodaImg.src = ('../js/images/sodaPixil.png')
+
+// const burgerImg = new Image()
+// burgerImg.src = ('../js/images/burgerPixil.png')
+
+// function gameElem(url, x, y) {
+//     this.url = url
+//     this.x = x
+//     this.y = y
+//     this.alive = true
+
+//     this.render = function () {
+//         ctx.drawImage(this.url, this.x, this.y,)
+//     }
+// }
+
+// let foodOne = new foodElem(foodImg, 200, 100)
+// let foodTwo = new foodElem(foodImg, 275, 45)
+// let foodThree = new foodElem(foodImg, 300, 130)
+// let trashOne = new gameElem(sodaImg, 75, 0)
+// let trashTwo = new gameElem(appleImg, 150, 0)
+// let trashThree = new gameElem(sodaImg, 215, 0)
+// let trashFour = new gameElem(burgerImg, 250, -15)
+
+// // allow player to move manatee around with with arrow keys
+// let movePlayer = (e) => {
+// switch (e.key) {
+//     // left arrow
+//     case ('ArrowLeft'):
+//         player.x -= player.speed
+//         if (player.x <= 25) {
+//             player.x = 25
+//             foodOne.x += 1
+//             foodTwo.x += 1
+//             foodThree.x += 1
+//             trashOne.x += 1
+//         }
+//         break
+//     // up arrow
+//     case ('ArrowUp'):
+//         player.y -= player.speed
+//         if (player.y <= 10) {
+//             player. y = 10
+//         }
+//         break
+//     // right arrow
+//     case ('ArrowRight'):
+//         player.x += player.speed
+//         if (player.x >= 245) {
+//             player.x = 245
+//             foodOne.x -= 1
+//             foodTwo.x -= 1
+//             foodThree.x -= 1
+//             trashOne.x -= 1
+//         }
+//         break
+//     // down arrow
+//     case ('ArrowDown'):
+//         player.y += player.speed
+//         if (player.y >= 135) {
+//             player.y = 135
+//         }
+//         break
+//     }
+// }
+
+// // have trash falling down
+// let trashMove = () => {
+//     trashOne.y += Math.random()
+//     if (trashOne.y >=175) {
+//         trashOne.y = 0
+//     }
+//     trashTwo.y += .33
+//     if (trashTwo.y >=225) {
+//         trashTwo.y = 0
+//     }
+//     trashThree.y += Math.random()
+//     if (trashThree.y >= 190) {
+//         trashThree.y = 0
+//     }
+//     trashFour.y += .25
+//     if (trashFour.y >= 175) {
+//         trashFour.y = -10
+//     } 
+// }
+
+// // create collsion detection for when player collects food
+// // const detectFoodEaten = () => {
+// //     if (
+// //         player.x < foodOne.x + foodOne.width &&
+// //         player.x + player.width > foodOne.x &&
+// //         player.y < foodOne.y + foodOne.height &&
+// //         player.y + player.height > foodOne.y
+// //     ) {
+// //         foodOne.alive = false
+// //     }
+// // }
+
+// function animate() {
+//     // clears canvas
+//     ctx. clearRect(0, 0, canvas.width, canvas.height)
+//     trashOne.render()
+//     // if (foodOne.alive) {
+//     //     foodOne.render()
+//     //     detectFoodEaten()
+//     // }
+//     foodOne.render()
+//     foodTwo.render()
+//     foodThree.render()
+//     trashTwo.render()
+//     trashThree.render()
+//     trashFour.render()
+//     trashMove()
+//     drawPlayer(manateeImg, player.x, player.y, player.width, player.height)
+//     requestAnimationFrame(animate)
+// }
+
+// animate()
+
+// document.addEventListener('keydown', movePlayer)
+
+
+
+
+// ARRAYS VERSION
 const game = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 const foodEaten = document.getElementById('btmRight')
@@ -18,6 +180,7 @@ const foodEaten = document.getElementById('btmRight')
 const manateeImg = new Image()
 manateeImg.src = ('../js/images/manatee.png')
 
+// create manatee
 function drawPlayer(img, dX, dY, dW, dH) {
     ctx.drawImage(img, dX, dY, dW, dH)
     this.alive = true
@@ -35,6 +198,42 @@ let player = {
 const foodImg = new Image()
 foodImg.src = ('../js/images/foodPixil.png')
 
+// food class constructor
+function foodElem(url, x, y) {
+    this.url = url
+    this.x = x
+    this.y = y
+    this.alive = true
+
+    this.render = function() {
+        ctx.drawImage(this.url, this.x, this.y)
+    }
+}
+
+// spawn 10 food items randomly on canvas
+let foodArr = []
+function addFood () {
+    for (i = 0; i <= 10; i++) {
+    let food = new foodElem(foodImg, (Math.random()*300), (Math.random()*150))
+    foodArr.push(food)
+    }
+}
+
+console.log(foodArr)
+
+// being called down in animate function
+function createFood() {
+    // if (foodArr.length <= 10) {
+        addFood()
+        let newFood = {}
+        console.log(newFood)
+        // Loop over each array to render each item
+        for (let i = 0; i < 10; i++) {
+            newFood = foodArr[i]
+            newFood.render()
+        }
+    }
+}
 const appleImg = new Image()
 appleImg.src = ('../js/images/applePixil.png')
 
@@ -44,105 +243,75 @@ sodaImg.src = ('../js/images/sodaPixil.png')
 const burgerImg = new Image()
 burgerImg.src = ('../js/images/burgerPixil.png')
 
-function gameElem(url, x, y) {
+function trashElem(url, x, y) {
     this.url = url
     this.x = x
     this.y = y
     this.alive = true
 
+    this.update = function() {
+        this.y += Math.random()
+    }
+
     this.render = function () {
-        ctx.drawImage(this.url, this.x, this.y,)
+        ctx.drawImage(this.url, this.x, this.y)
     }
 }
 
-const trash = [appleImg, sodaImg, burgerImg]
+let trashArr = []
 
-for (let i = 0; i<trash.length; i++) {
-    new gameElem(trash[i], (Math.floor(Math.random()*300)))
-}
+// function spawnTrash() {
+//     setInterval(() => {
+//         const url = burgerImg
+//         const x = 200
+//         const y = 0
+//         const velocity = {
+//             x: 1,
+//             y: 1
+//         }
+//         trashArr.push(new gameElem(url, x, y, velocity))
 
-let foodOne = new gameElem(foodImg, 200, 100)
-let foodTwo = new gameElem(foodImg, 275, 45)
-let foodThree = new gameElem(foodImg, 300, 130)
-let trashOne = new gameElem(sodaImg, 75, 0)
-let trashTwo = new gameElem(appleImg, 150, 0)
-let trashThree = new gameElem(sodaImg, 215, 0)
-let trashFour = new gameElem(burgerImg, 250, -15)
+//         // console.log(trashArr)
+//     }, 3000)
+// } 
 
-// allow player to move manatee around with with arrow keys
-let movePlayer = (e) => {
-switch (e.key) {
-    // left arrow
-    case ('ArrowLeft'):
-        player.x -= player.speed
-        if (player.x <= 25) {
-            player.x = 25
-            foodOne += 1
-            foodTwo.x += 1
-            trashOne.x += 1
-        }
-        break
-    // up arrow
-    case ('ArrowUp'):
-        player.y -= player.speed
-        if (player.y <= 10) {
-            player. y = 10
-        }
-        break
-    // right arrow
-    case ('ArrowRight'):
-        player.x += player.speed
-        if (player.x >= 245) {
-            player.x = 245
-            foodOne.x -= 1
-            foodTwo.x -= 1
-            trashOne.x -= 1
-        }
-        break
-    // down arrow
-    case ('ArrowDown'):
-        player.y += player.speed
-        if (player.y >= 135) {
-            player.y = 135
-        }
-        break
-    }
-}
-
-// have trash falling down
-let trashMove = () => {
-    trashOne.y += Math.random()
-    if (trashOne.y >=175) {
-        trashOne.y = 0
-    }
-    trashTwo.y += .33
-    if (trashTwo.y >=225) {
-        trashTwo.y = 0
-    }
-    trashThree.y += Math.random()
-    if (trashThree.y >= 190) {
-        trashThree.y = 0
-    }
-    trashFour.y += .25
-    if (trashFour.y >= 175) {
-        trashFour.y = -10
-    } 
-}
-
-// create collsion detection for when player collects food
-// const detectFoodEaten = () => {
-//     if (
-//         player.x < foodOne.x + foodOne.width &&
-//         player.x + player.width > foodOne.x &&
-//         player.y < foodOne.y + foodOne.height &&
-//         player.y + player.height > foodOne.y
-//     ) {
-//         foodOne.alive = false
+// let trashMove = () => {
+//     trashOne.y += Math.random()
+//     if (trashOne.y >=175) {
+//         trashOne.y = 0
 //     }
 // }
 
-// create separate arrays for food and trash to populate
-// Loop over each array to render each item
+// allow player to move manatee with arrow keys
+let movePlayer = (e) => {
+    switch (e.key) {
+        case ('ArrowLeft'):
+            player.x -= player.speed
+            if (player.x <= 25) {
+                player.x = 25
+            }
+            break
+        case ('ArrowUp'):
+            player.y -= player.speed
+            if (player.y <= 10) {
+                player. y = 10
+            }
+            break
+        case ('ArrowRight'):
+            player.x += player.speed
+            if (player.x >= 245) {
+                player.x = 245
+            }
+            break
+        case ('ArrowDown'):
+            player.y += player.speed
+            if (player.y >= 135) {
+                player.y = 135
+            }
+            break
+        }
+    }
+
 // Refactor my collision detection so that it has a param to reuse for multiple items
 // Loop over food array and call detection on each of those
 // Splice the food from the array
@@ -150,34 +319,12 @@ let trashMove = () => {
 function animate() {
     // clears canvas
     ctx. clearRect(0, 0, canvas.width, canvas.height)
-    trashOne.render()
-    // if (foodOne.alive) {
-    //     foodOne.render()
-    //     detectFoodEaten()
-    // }
-    foodOne.render()
-    foodTwo.render()
-    foodThree.render()
-    trashTwo.render()
-    trashThree.render()
-    trashFour.render()
-    trashMove()
+    createFood()
     drawPlayer(manateeImg, player.x, player.y, player.width, player.height)
     requestAnimationFrame(animate)
 }
 
 animate()
-
-// const gameLoop = () => {
-//     ctx.clearRect(0, 0, game.width, game.height)
-//     // if (food.alive) {
-//     //     food.render()
-//     //     detectFoodEaten()
-//     // }
-//     // trash.render()
-//     player.render()
-// }
+// spawnTrash()
 
 document.addEventListener('keydown', movePlayer)
-
-// let gameInterval = setInterval(gameLoop, 70)
